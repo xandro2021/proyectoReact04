b1b0c08b09896ac9d0ca08319890a5fcb33c8dda
xandro2021 <xandrocastro2021@gmail.com>
2023-10-09 20:42:26 -0600

    Add notes about Link and NavLink & Give extra styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,8 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="md:flex md:min-h-screen">
 
@@ -8,8 +10,17 @@ const Layout = () => {
         <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
 
         <nav className="mt-10">
-          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/">Clientes</Link>
-          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/clientes/nuevo">Nuevo Cliente</Link>
+
+          <Link
+            className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+            to="/"
+          >Clientes</Link>
+
+          <Link
+            className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+            to="/clientes/nuevo"
+          >Nuevo Cliente</Link>
+
         </nav>
       </aside>
 
