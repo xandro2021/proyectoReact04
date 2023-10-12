f3fd5eddd837e29ce44567ae954a38ea06bf52ff
xandro2021 <xandrocastro2021@gmail.com>
2023-10-12 10:39:05 -0600

    Use the hook loader from react-router-dom to retrieve information

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,12 @@
+function Cliente({ cliente }) {
+
+  const { nombre, empresa, email, telefono, id } = cliente;
+
+  return (
+    <tr>
+      <td className="p-6">{nombre}</td>
+    </tr>
+  );
+}
+
+export default Cliente;
