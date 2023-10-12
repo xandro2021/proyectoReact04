3c38211084d0a3e3700f486740b1d8f7a19631ce
xandro2021 <xandrocastro2021@gmail.com>
2023-10-11 19:29:05 -0600

    Introduction to the concept of loader in React

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,17 @@
+export function loader() {
+
+  return 'Desde Loader';
+}
+
 const Index = () => {
   return (
-    <div>Clientes</div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+
+      <p className="mt-3">
+        Administra tus Clientes
+      </p>
+    </>
   );
 };
 
