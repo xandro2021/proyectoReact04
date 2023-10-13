232f2ee3942861d6df03cda4cfd0282f8867b7e3
xandro2021 <xandrocastro2021@gmail.com>
2023-10-12 18:30:42 -0600

    Introduction to actions for Forms

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Layout from './components/Layout';
-import NuevoCliente from './pages/NuevoCliente';
+import NuevoCliente, { action as nuevoClienteAction } from './pages/NuevoCliente';
 import Index, { loader as clientesLoader } from './pages/Index';
 
 const router = createBrowserRouter([
@@ -19,7 +19,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/clientes/nuevo',
-        element: <NuevoCliente />
+        element: <NuevoCliente />,
+        action: nuevoClienteAction
       }
     ]
   },
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,11 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Form } from 'react-router-dom';
+import Formulario from '../components/Formulario';
+
+export function action() {
+  console.log('Submit al Formulario');
+
+  return null;
+}
 
 const NuevoCliente = () => {
 
@@ -17,10 +24,20 @@ const NuevoCliente = () => {
         >Volver</button>
       </div>
 
-      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        <p>
-          Formulario Aqui
-        </p>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-10">
+
+        <Form
+          method="post"
+        >
+          <Formulario />
+
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </Form>
+
       </div>
     </>
   );
