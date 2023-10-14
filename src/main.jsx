7ca188fbec0d7149e7c2af5b1f5aa933d267a365
xandro2021 <xandrocastro2021@gmail.com>
2023-10-14 15:50:43 -0600

    add a loader hook to retrieve the client id

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 import NuevoCliente, { action as nuevoClienteAction } from './pages/NuevoCliente';
 import Index, { loader as clientesLoader } from './pages/Index';
+import EditarCliente, { loader as editarClienteLoader } from './pages/EditarCliente';
 import ErrorPage from './components/ErrorPage';
 
 const router = createBrowserRouter([
@@ -23,6 +24,11 @@ const router = createBrowserRouter([
         path: '/clientes/nuevo',
         element: <NuevoCliente />,
         action: nuevoClienteAction
+      },
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente />,
+        loader: editarClienteLoader
       }
     ]
   },
