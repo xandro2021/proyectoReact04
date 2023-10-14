51e7943f91767834a11dcc3ddd6db25d3ce8ca18
xandro2021 <xandrocastro2021@gmail.com>
2023-10-14 11:59:31 -0600

    Guardar los datos en el json-server

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,7 @@
-import { useNavigate, Form, useActionData } from 'react-router-dom';
+import { useNavigate, Form, useActionData, redirect } from 'react-router-dom';
 import Formulario from '../components/Formulario';
 import Error from '../components/Error';
+import { agregarCliente } from '../data/clientes';
 
 export async function action({ request }) {
 
@@ -28,7 +29,9 @@ export async function action({ request }) {
     return errores;
   }
 
-  return null;
+  await agregarCliente(datos);
+
+  return redirect('/');
 }
 
 const NuevoCliente = () => {
